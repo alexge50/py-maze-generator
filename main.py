7bb20f1d380c5ecb0e3d97568895db797bd69d7d
alexge50 <alexge50.x@gmail.com>
2017-09-18 00:22:58 +0300

    uses ncurses

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import maze
+import curses
+
+import sys, getopt
 
 
-def print_maze(maze, w, h):
-
+def print_maze(stdscr, maze, w, h):
     wall = '█'
     empty = ' '
 
@@ -24,13 +26,53 @@ def print_maze(maze, w, h):
                 maze_displayable[2 * y + 1][2 * x + 2] = wall
                 maze_displayable[2 * y + 2][2 * x + 2] = wall
 
+    line = 0
+
     for y in maze_displayable:
         string = ""
         for x in y:
             string = string + x
-        print("{}".format(string))
+        stdscr.addstr(line, 0, string, curses.color_pair(1))
+        line = line + 1
 
 
-maze, w, h = maze.generate_maze(20, 20, 150)
+seed = None
 
-print_maze(maze, w, h)
+
+def main(stdscr):
+    y, x = stdscr.getmaxyx()
+
+    global seed
+
+    if seed is None:
+        seed = 10
+
+    _maze, w, h = maze.generate_maze(int((x - 1) / 2), int((y - 1) / 2), 150)
+
+    curses.flash()
+    stdscr.scrollok(True)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+
+    print_maze(stdscr, _maze, w, h)
+    stdscr.refresh()
+
+    stdscr.addstr(y - 1, 0, "seed: {}".format(seed), curses.color_pair(0))
+
+    exit_flag = 0
+    while exit_flag == 0:
+        if stdscr.getkey() == 'q':
+            exit_flag = 1
+
+
+try:
+    opts, args = getopt.getopt(sys.argv, "s:", ["seed="])
+except getopt.GetoptError:
+    print("main.py -s <number>")
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ("-s", "--seed"):
+        global seed
+        seed = int(args)
+
+curses.wrapper(main)
